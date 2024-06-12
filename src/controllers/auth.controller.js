b2faba0e7ef59e9c7ab8116313e0c7ff92755da5
ismail <ismailbary2@gmail.com>
2024-06-12 14:18:40 +0800

    feature: add register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,9 +3,36 @@ const jwt = require("jsonwebtoken")
 
 const { user: UserModel } = require("../models")
 
+const register = async(req, res, next) => {
+    const { nama, email, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    await UserModel.create({
+            nama,
+            email,
+            password: passwordHash,
+        })
+        .then((user) => {
+            if (!user) {
+                return res.status(500).send({
+                    message: "Failed to register user",
+                    data: null,
+                });
+            }
+
+            return res.send({
+                message: "User successfully registered",
+                data: null,
+            });
+        })
+        .catch((err) => {
+            next(err);
+        });
+};
+
 const login = async(req, res, next) => {
 
-    const { name, email, password } = req.body;
+    const { email, password } = req.body;
 
     const user = await UserModel.findOne({ where: { email } })
     if (!user) {
@@ -33,5 +60,6 @@ const login = async(req, res, next) => {
 }
 
 module.exports = {
-    login
+    login,
+    register
 }
--- src/routes/auth.root.js
+++ src/routes/auth.root.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const { login } = require("../controllers/auth.controller")
+const { login, register } = require("../controllers/auth.controller")
 
 // /api/babs
 router.post("/login", login);
+router.post("/register", register);
 
 
 module.exports = router;
