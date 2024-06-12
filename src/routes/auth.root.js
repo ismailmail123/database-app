fa77d8392acf3bf5d0e524172ca5904212114457
ismail <ismailbary2@gmail.com>
2024-06-12 13:43:35 +0800

    feature: add validator email n password

--- src/routes/auth.root.js
+++ src/routes/auth.root.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { login, register, removeUser } = require("../controllers/auth.controller")
+
+const { login, register, removeUser } = require("../controllers/auth.controller");
+const { validateLogin, validateRegister } = require("../middlewares/validator");
 
 // /api/babs
-router.post("/login", login);
-router.post("/register", register);
+router.post("/login", validateLogin, login);
+router.post("/register", validateRegister, register);
 router.delete("/delete/:id", removeUser)
 
 
