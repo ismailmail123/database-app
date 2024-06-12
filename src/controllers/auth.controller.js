0de07f6b70b6d987e4c5333e62838016f898cf51
ismail <ismailbary2@gmail.com>
2024-06-12 13:23:02 +0800

    feature: check email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,26 @@
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-
-const { user: UserModel } = require("../models")
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { user: UserModel } = require("../models");
 
 const register = async(req, res, next) => {
     const { nama, email, password } = req.body;
 
-    const passwordHash = await bcrypt.hash(password, 10);
-    await UserModel.create({
-            nama,
-            email,
-            password: passwordHash,
-        })
-        .then((user) => {
+    try {
+        //pengecekan email
+        const userExist = await UserModel.findOne({ where: { email } });
+        if (userExist) {
+            return res.status(401).json({ message: "Email already exist" });
+        } else {
+            //create new user
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const user = await UserModel.create({
+                nama,
+                email,
+                password: passwordHash,
+            });
+
+
             if (!user) {
                 return res.status(500).send({
                     message: "Failed to register user",
@@ -24,42 +32,71 @@ const register = async(req, res, next) => {
                 message: "User successfully registered",
                 data: null,
             });
-        })
-        .catch((err) => {
-            next(err);
-        });
+        }
+    } catch (err) {
+        next(err);
+    }
 };
 
 const login = async(req, res, next) => {
-
     const { email, password } = req.body;
 
-    const user = await UserModel.findOne({ where: { email } })
-    if (!user) {
-        return res.status(401).json({ message: "Invalid email/password" })
-    }
+    try {
+        const user = await UserModel.findOne({ where: { email } });
 
-    const isValid = await bcrypt.compare(password, user.password); //(password yang dikirim, password dari database)
-    if (!isValid) {
-        return res.status(401).json({ message: "Invalid email/password" })
-    }
-
-    const data = {
-        id: user.id,
-        email: user.email
-    }
-    const token = jwt.sign(data, process.env.JWT_SECRET); //(data yg dikirim, data dari jwt secret)
-
-    return res.send({
-        message: "Login success fully",
-        data: {
-            token: token, //bisa dengan token saja
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email/password" });
         }
-    })
 
-}
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            return res.status(401).json({ message: "Invalid email/password" });
+        }
+
+        const data = {
+            id: user.id,
+            email: user.email,
+        };
+        const token = jwt.sign(data, process.env.JWT_SECRET);
+
+        return res.send({
+            message: "Login successful",
+            data: {
+                token: token,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const removeUser = async(req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const user = await UserModel.findByPk(id);
+
+        if (!user) {
+            return res.status(404).send({
+                message: "User not found",
+                data: null,
+            });
+        }
+
+        await UserModel.destroy({ where: { id } });
+
+        return res.send({
+            message: "User successfully removed",
+            data: null,
+        });
+    } catch (err) {
+        next(err);
+    }
+};
 
 module.exports = {
     login,
-    register
-}
+    register,
+    removeUser,
+};
--- src/routes/auth.root.js
+++ src/routes/auth.root.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const { login, register } = require("../controllers/auth.controller")
+const { login, register, removeUser } = require("../controllers/auth.controller")
 
 // /api/babs
 router.post("/login", login);
 router.post("/register", register);
+router.delete("/delete/:id", removeUser)
 
 
 module.exports = router;
